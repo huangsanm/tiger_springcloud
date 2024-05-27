3e3133933158a70aecf960d35fd6f29e340337dd
huangsanm <huangsanm@gmail.com>
2024-05-27 10:26:28 +0800

    初次提交

--- tiger_gateway/src/main/java/com/tiger/tigergateway/TigerGatewayApplication.java
+++ tiger_gateway/src/main/java/com/tiger/tigergateway/TigerGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.tiger.tiger_gateway;
+package com.tiger.tigergateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
