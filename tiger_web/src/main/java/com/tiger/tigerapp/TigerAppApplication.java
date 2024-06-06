8a0791c7e3dde62034ee28d5278cc9690cff4b8c
huangsanm <huangsanm@gmail.com>
2024-06-06 15:23:39 +0800

    集成降级熔断、监控

--- tiger_web/src/main/java/com/tiger/tigerapp/TigerAppApplication.java
+++ tiger_web/src/main/java/com/tiger/tigerapp/TigerAppApplication.java
@@ -3,13 +3,11 @@ package com.tiger.tigerapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableHystrix
-@EnableTurbine
 public class TigerAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(TigerAppApplication.class, args);
